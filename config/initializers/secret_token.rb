03bf3e5e3b4a3aec4868fb5f049db6e14adbe80c
biguo <as2961@126.com>
2015-01-03 11:54:13 +0800

    second  commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OperationPortal::Application.config.secret_key_base = '422af712e87ff921a986de94d7bef585db3980f6837dfd65969fb1d29a3034929b6b367367e6ca694d2f69a88627ef8ef479cd5a54f7aaa969ae4fd775b5abb4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OperationPortal::Application.config.session_store :cookie_store, key: '_operation-portal_session'
